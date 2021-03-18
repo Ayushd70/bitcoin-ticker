77012059e85555ba982650a336e1062834c410a2
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-18 21:35:14 +0530

    bitcoin-ticker: initial push
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ android/app/src/main/java/co/ayushd70/bitcoin_ticker/MainActivity.java
@@ -0,0 +1,13 @@
+package co.ayushd70.bitcoin_ticker;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
