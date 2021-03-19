95f0ffe93403b7753f1f38ac7b8f25f1a28a8eb3
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-19 22:28:55 +0530

    bitcon-ticker: implement a initial dropdown menu
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +44,28 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: [
+                DropdownMenuItem(
+                  child: Text('USD'),
+                  value: 'USD',
+                ),
+                DropdownMenuItem(
+                  child: Text('EUR'),
+                  value: 'EUR',
+                ),
+                DropdownMenuItem(
+                  child: Text('INR'),
+                  value: 'INR',
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value;
+                });
+              },
+            ),
           ),
         ],
       ),
