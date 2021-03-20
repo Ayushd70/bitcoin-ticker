5bd35622870392630088416262632a94939d00be
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-20 22:39:28 +0530

    bitcoin-ticker: simplfy the widget picker
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -54,14 +54,6 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  Widget getPicker() {
-    if (Platform.isIOS) {
-      return iOSPicker();
-    } else if (Platform.isAndroid) {
-      return androidDropdown();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,7 +90,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
