5053f1b995297dc20de6958a2ac8e50131b55e82
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-21 21:49:37 +0530

    bitcoin-ticker: try pulling the value of BTC in USD
    *initial api application
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,21 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'FA75A329-CB45-4124-BFF5-ED752B777CE8';
+
+class CoinData {
+  Future getCoinData() async {
+    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    http.Response response = await http.get(Uri.parse(requestURL));
+
+    if (response.statusCode == 200) {
+      var decodeData = jsonDecode(response.body);
+      var lastPrice = decodeData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Error getting the requested url';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -54,6 +54,25 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
+  String bitcoinValueInUSD = '?';
+
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,7 +94,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValueInUSD USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
