9ad8194f3d6099dcc36b94b396c31ddc69880e9a
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-23 21:25:24 +0530

    bitcoin-ticker: UI: update background scaffold color
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-          primaryColor: Color(0xFF0A0E21),
-          scaffoldBackgroundColor: Colors.white),
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: PriceScreen(),
     );
   }
