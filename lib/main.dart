fa1902f62c3d452acc395bc16793c2e16b519851
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-23 21:23:55 +0530

    bitcoin-ticker: UI: update primary color
    *use 0xFF0A0E21
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'price_screen.dart';
 
 void main() => runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
+          primaryColor: Color(0xFF0A0E21),
           scaffoldBackgroundColor: Colors.white),
       home: PriceScreen(),
     );
